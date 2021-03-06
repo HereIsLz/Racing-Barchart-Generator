2fc99bb94baea62487411de5644b71ad33d81aaf
HereIsLz <master_lee@live.com>
2021-03-06 10:00:28 +0800

    ui shell

---
+++ precache-manifest.893f8a3a13f5f9cfbcaeca59951fa19f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8e38215c3f216082bb8e4d6258f39fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6070e5cec0e8d5922489",
+    "url": "/static/css/main.a09e832c.chunk.css"
+  },
+  {
+    "revision": "30827932458663c91c15",
+    "url": "/static/js/2.eb87ad03.chunk.js"
+  },
+  {
+    "revision": "e7856d5c439b7b6b344d051552deb326",
+    "url": "/static/js/2.eb87ad03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6070e5cec0e8d5922489",
+    "url": "/static/js/main.8972e165.chunk.js"
+  },
+  {
+    "revision": "821c7dd905d8233a9304",
+    "url": "/static/js/runtime-main.e13b62d8.js"
+  }
+]);
